8678aa13eba17c0512a39af312b11cdd882aa1fb
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-10-26 21:41:35 +0200

    Version 1.0.0-beta.2

---
+++ src/App/Models/DownloadFileModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Netopes.Core.App.Models
+{
+    public class DownloadFileModel
+    {
+        public Guid FileId { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public byte[] FileContent { get; set; }
+
+        [JsonIgnore]
+        public string FileNameWithExtension => string.IsNullOrWhiteSpace(FileName) ? $"{DateTime.Now:yyyyMMddHHmmss}{FileExtension}" : $"{FileName}{FileExtension}";
+
+        [JsonIgnore]
+        public string FileContentType
+        {
+            get
+            {
+                return FileExtension.Trim('.').ToLower() switch
+                {
+                    "xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    _ => "application/octet-stream"
+                };
+            }
+        }
+    }
+}
